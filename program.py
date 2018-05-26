7cbb127ca91a19a73d0ed33b95ce340011777ef4
CY2020 <39651669+CY2020@users.noreply.github.com>
2018-05-26 15:53:13 -0400

    Create program.py

---
+++ program.py
@@ -0,0 +1,52 @@
+#Mobile Emergency
+#MakeSPPrep Project
+#Heart Health
+print "Welcome, please insert your health information."
+print "Please select one of the following conditions:"
+print "1. arrhythmia"
+print "2. high blood pressure"
+print "3. history cardic arrest"
+print "4. heart murmurs"
+print "5. coronary hartry disease"
+print "6. congestive heart failure"
+print "7. periphial artery disease"
+print "8. stroke"
+print "9. prior heart attacks"
+print "10. cogenital heart disease"
+information = raw_input()
+if information == 'arrhythmia':
+    print "We will monitor you heart rate, in case of emergency, we will contact the paramedics."
+elif information == 'high blood pressure':
+    print "We will remind you what to avoid and any habits you fall victim to."
+elif information == 'history cardiac arrest':
+    print "We will constantly make sure your heart is stable."
+elif information == 'heart murmurs':
+    print "We recommend that you visit your doctor often"
+elif information == 'coronary artery disease':
+    print "We recommend that you try to lower your blood pressure to prevent any heart attacks." 
+elif information == 'congestive heart failure':
+    print "Go see your doctor to finalize the cause of you problem."
+elif information == 'periphial artery disease':
+    print "Lessen use of tobacco, get more exercise, eat healthy."
+elif information == 'stroke':
+    print "Go take blood thinners as well as tPa."
+elif information == 'prior heart attacks':
+    print "What is recommended is that you take aspirin to prevent blood clots that could lead to a heart attack."
+elif information == 'cogenital heart disease':
+    print "what you should do is to lower your blood pressure by any means"
+else:
+    print "I'm sorry, that is not an option."
+    
+print "What is your current mood? Stressed or calm?"
+mood = raw_input()
+if mood == 'stressed':
+    print "We recommend that you take cation of your health and be vigilant of any symptoms of diseases."
+elif mood == 'calm':
+    print "Then you are not in danger of any conditions or high blood pressure."
+else:
+    print "I'm sorry, that is not an option."
+    
+    
+ print "For more information contact: yanezc20@students.spprep.org"
+ 
+    
